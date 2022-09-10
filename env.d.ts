702cb29e0f360a7f7e6ba2b2d2c1c7d14667a4b9
Harivansh <hello@harivan.sh>
2022-09-10 16:35:15 +0400

    build: add environment example file & comment env variables

--- env.d.ts
+++ env.d.ts
@@ -1,11 +1,26 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  /**
+   * The url of the minitick server.
+   */
   readonly VITE_SERVER_URL: string;
 
+  /**
+   * Refresh the access-token the specified amount of time before the
+   * token expires.
+   */
   readonly VITE_ACCESS_TOKEN_REFRESH_THRESHOLD: string;
+  /**
+   * Refresh the refresh-token the specified amount of time before the
+   * token expires.
+   */
   readonly VITE_REFRESH_TOKEN_REFRESH_THRESHOLD: string;
 
+  /**
+   * How long to wait before retrying to refresh a token if the refresh
+   * operation fails.
+   */
   readonly VITE_TOKEN_REFRESH_RETRY_INTERVAL: string;
 }
 
